45d6d7a542b21e5b54ea0f68f380a2b54504ccae
Efilrofzaggin4 <maxime.kiniffo@3wa.io>
2024-12-10 11:19:42 +0100

    first commit

--- script.js
+++ script.js
@@ -1,62 +1,83 @@
 const pokemons = [
-    {
-        id: 1,
-        imgUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/1.png",
-        nom: "Bulbizarre",
-    },
-    {
-        id: 4,
-        imgUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/4.png",
-        nom: "Salamèche",
-    },
-    {
-        id: 7,
-        imgUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/7.png",
-        nom: "Carapuce",
-    },
+  {
+    id: 1,
+    imgUrl:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/1.png",
+    nom: "Bulbizarre",
+  },
+  {
+    id: 4,
+    imgUrl:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/4.png",
+    nom: "Salamèche",
+  },
+  {
+    id: 7,
+    imgUrl:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/7.png",
+    nom: "Carapuce",
+  },
 ];
 
-window.onload = function() {
-    
-}
+window.onload = function () {
+  let showPokedex = document.querySelector("#show-pokedex");
+
+  showPokedex.addEventListener("click", () => {
+    afficherListePokemons();
+  });
+};
 
 function afficherListePokemons() {
-    const conteneur = document.querySelector("main > section .conteneur");
-    for (let i = 0; i < pokemons.length; i++) {
-        const pokemon = pokemons[i];
-        const divPokemon = creerDivPokemonListe(pokemon);
-        conteneur.append(divPokemon);
-    }
+  const conteneur = document.querySelector("main > section .conteneur");
+
+  conteneur.innerHTML = "";
+  for (let i = 0; i < pokemons.length; i++) {
+    const pokemon = pokemons[i];
+    const divPokemon = creerDivPokemonListe(pokemon);
+    conteneur.append(divPokemon);
+  }
 }
 
 function creerDivPokemonListe(pokemon) {
-    const div = document.createElement("div");
-    div.classList.add("div-pokemon-liste");
-    div.setAttribute("data-pokemon-id", pokemon.id);
+  const div = document.createElement("div");
+  div.classList.add("div-pokemon-liste");
+  div.setAttribute("data-pokemon-id", pokemon.id);
 
-    const img = document.createElement("img");
-    img.src = pokemon.imgUrl;
+  const img = document.createElement("img");
+  img.src = pokemon.imgUrl;
 
-    const spanNom = document.createElement("span");
-    spanNom.innerText = pokemon.nom;
+  const spanNom = document.createElement("span");
+  spanNom.innerText = pokemon.nom;
 
-    const spanId = document.createElement("span");
-    spanId.innerText = pokemon.id;
+  const spanId = document.createElement("span");
+  spanId.innerText = pokemon.id;
 
-    div.append(img);
-    div.append(spanNom);
-    div.append(spanId);
+  div.append(img);
+  div.append(spanNom);
+  div.append(spanId);
 
-    return div;
+  div.addEventListener("click", () => {
+    afficherPokemon(pokemon);
+  });
+
+  return div;
 }
 
 function creerDivPokemonListeVersionInnerHTML(pokemon) {
-    const div = document.createElement("div");
-    div.classList.add("div-pokemon-liste");
-    div.innerHTML = `
+  const div = document.createElement("div");
+  div.classList.add("div-pokemon-liste");
+  div.innerHTML = `
         <img src="${pokemon.imgUrl}">
         <span>${pokemon.nom}</span>
         <span>${pokemon.id}</span>
     `;
-    return div;
+  return div;
+}
+
+function afficherPokemon(pokemon) {
+  const div = creerDivPokemonListe(pokemon);
+
+  const conteneur = document.querySelector("main > section .conteneur");
+  conteneur.innerHTML = "";
+  conteneur.append(div);
 }
